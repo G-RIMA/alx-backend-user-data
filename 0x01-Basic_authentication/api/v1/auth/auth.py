00469e49f54f3313afc48642ae2f03d4ba319f2b
G-RIMA <gikandimaria178@gmail.com>
2023-02-10 16:43:46 +0300

    created auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from flask import request
+from typing import TypeVar, List
+
+class Auth:
+    """ 
+    Authentication class 
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ 
+        require authentication 
+        """
+        return False
+
+    
+    def authorization_header(self, request=None) -> str:
+        """ 
+        Authorisation Header
+        """
+        return None
+
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user method """
+        return None
