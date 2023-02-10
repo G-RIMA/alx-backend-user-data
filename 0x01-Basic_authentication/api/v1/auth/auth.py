9f052b842d955e1561f4d978d3d75223b9c356a3
G-RIMA <gikandimaria178@gmail.com>
2023-02-10 17:21:19 +0300

    added new line

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,25 +6,24 @@ API authentication
 from flask import request
 from typing import TypeVar, List
 
+
 class Auth:
-    """ 
-    Authentication class 
+    """Authentication class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ 
-        require authentication 
+        """
+        require authentication
         """
         return False
 
-    
     def authorization_header(self, request=None) -> str:
-        """ 
+        """
         Authorisation Header
         """
         return None
 
-    
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        current user method """
+        current user method
+        """
         return None
