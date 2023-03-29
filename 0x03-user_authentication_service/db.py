7f667e4bb733ff399d9c26879f16f8cd164a8cc8
G-RIMA <gikandimaria178@gmail.com>
2023-03-29 10:15:48 +0300

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,3 +41,13 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kargs) -> TypeVar('User'):
+        ''' def find user '''
+        try:
+            user = self._session.query(User).filter_by(**kargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        return user
